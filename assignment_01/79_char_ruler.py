4913e45fbacc3a20c95bb52742b9bf3d2be8673a
Kenglychu Xiong <kenxiong@unc.edu>
2025-05-22 20:12:08 -0400

    79 Character Ruler Test

---
+++ assignment_01/79_char_ruler.py
@@ -0,0 +1,21 @@
+# Kenglychu Xiong
+# This is a screenshot of the 79 char ruler.
+
+# Max characters is 79
+max_char = 79
+
+# This will print out the number 79
+print(max_char)
+
+# This will print out 0 (as a string) * 79 times.
+print('0' * max_char)
+
+# Your printout will not match the line becuase the termial font is not the 
+# same size as the VS code font. But if you copy it and paste it will fit!
+
+"""
+0000000000000000000000000000000000000000000000000000000000000000000000000000000
+"""
+
+# This will print out the integer zero.
+print(0*79)
