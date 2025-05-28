5b7a00e017f02296a70164a1baceded0bb00b0e0
Kenglychu Xiong <kenxiong@unc.edu>
2025-05-27 20:09:38 -0400

    Assignment 03 But Fixed

--- assignment_03/main.py
+++ assignment_03/main.py
@@ -7,14 +7,14 @@ USAB_ACC = "Y"    # The existence of a USA Boxing Account
 # Get the Boxer's experience.
 exp = int(input('Enter the amount of boxing experience in months: '))
 
-# Get the number of years on the current job.
+# Get the existence of the USAB Account
 acc = str(input('Do you have a USA Boxing Account? Y or N: ').strip().upper())
 
-# Determine whether the customer qualifies.
+# Determine whether the boxer qualifies for sparring training.
 if exp >= MIN_EXP and acc == USAB_ACC:
     print('You qualify for sparring! Good Luck!')
 else:
-    print(f'''You do not qualify sparring.\n
+    print(f'''You do not qualify for sparring.\n
           The minimum qualifications are: \n 
           -  {MIN_EXP} Months of Experience \n
           -  A USA Boxing Account''')
